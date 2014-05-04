fe9fca2a435e51f9369c08927656423ecbb78961
chinesedfan <chinesedfan@qq.com>
2014-05-04 22:15:30 +0800

    add class wins

--- js/arena.js
+++ js/arena.js
@@ -1,4 +1,4 @@
-function showArenaTrend(container, lineTicks, lineData) {
+function showArenaTrend(container, lineTicks, winData) {
     var lineOptions = {
         width: 900,
         height: 400,
@@ -10,7 +10,7 @@ function showArenaTrend(container, lineTicks, lineData) {
             y: {
                 min: 0,
                 max: 11,
-		total: 11,
+                total: 11,
                 tickSize: 2,
                 tickWidth: 20,
                 rotate: 90,
@@ -24,7 +24,40 @@ function showArenaTrend(container, lineTicks, lineData) {
             0: "circle",
         },
     };
-
+    var lineData = [{name: 0, data: winData}];
     var line = new Venus.SvgChart(container, lineData, lineOptions);
     return line;
+}
+
+function showClassWins(container, winData) {
+    var barTicks = ["Druid", "Hunter", "Mage", "Paladin", "Priest",
+        "Rogue", "Shaman", "Warlock", "Warrior"];
+    var barOptions = {
+        width: 900,
+        height: 400,
+        axis: {
+            x: {
+                tickWidth: 75,
+                ticks: barTicks,
+            },
+            y: {
+                min: 0,
+                max: 11,
+                total: 11,
+                tickSize: 2,
+                tickWidth: 20,
+                rotate: 90,
+            },
+        },
+        bar: {
+            radius: 0,
+        },
+    };
+    var arr = {};
+    for (var i = 0; i < barTicks.length; i++) {
+        arr[barTicks[i]] = winData[i];
+    }
+    var barData = [{name: 0, data: arr}];
+    var bar = new Venus.SvgChart(container, barData, barOptions);
+    return bar;
 }
