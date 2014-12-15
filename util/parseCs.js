8e5ac248609a4bc9ff842cc4df42a24367262daa
chinesedfan <chinesedfan@qq.com>
2014-12-15 11:45:18 +0800

    use re to match, fix blank enum value, rm an arg

--- util/parseCs.js
+++ util/parseCs.js
@@ -2,28 +2,32 @@
 	var Fs = require('fs');
 
 	function printHelp() {
-		console.log('Usage: node parseCs.js <csharp_file> <output_file>');
+		console.log('Usage: node parseCs.js <csharp_file>');
 	}
 
 	// The first is 'node', and the second is this file's full path.
 	var args = process.argv.splice(2);
-	if (args.length != 2) {
+	if (args.length != 1) {
 		printHelp();
 		return;
 	}
 
 	var content = Fs.readFileSync(args[0], 'utf-8'),
 	    lines = content.split('\n'),
-	    line, key, value, index;
-	console.log('var TagMap={');
+	    line, key, value, curVal = 0,
+	    reg = /^\s*([A-Z_][A-Z0-9_]+)(?:\s*=\s*(\d+))?,?\s*$/, matches;
+	console.log('var TagMap = {');
 	for (var i = 0; i < lines.length; i++) {
 		line = lines[i];
-		if (line.length < 2 || line[0] != '\t' || line[line.length - 1] != ',') continue;
+		if (!reg.test(line)) continue;
 
-		index = line.indexOf(' = ');
-		if (index < 0) continue;
-		key = line.substring(1, index);
-		value = line.substring(index + 3, line.length - 1);
+		matches = reg.exec(line);
+		key = matches[1];
+		value = matches[2];
+		if (!value) {
+			value = curVal;
+		}
+		curVal = parseInt(value) + 1;
 		console.log('\t\'' + value + '\': \'' + key + '\',');
 	}
 	console.log('};');
