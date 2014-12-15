0e9741ad00cc3c0aaa4223929f9f3c272c798f10
chinesedfan <chinesedfan@qq.com>
2014-12-15 18:13:24 +0800

    add parseXml.js

---
+++ util/parseXml.js
@@ -0,0 +1,47 @@
+(function () {
+	var Fs = require('fs');
+	var TagMap = require('../bin/tagmap.js');
+	// run 'npm install <module> -g' if need
+	var DOMParser = require('xmldom').DOMParser;
+
+	function printHelp() {
+		console.log('Usage: node parseXml.js <xml_file>');
+	}
+
+	// The first is 'node', and the second is this file's full path.
+	var args = process.argv.splice(2);
+	if (args.length != 1) {
+		printHelp();
+		return;
+	}
+
+	function node2obj(entity) {
+		var obj = {}, key,
+		    tags = entity.getElementsByTagName('Tag'),
+		    tid, type, value, text;
+		for (var i = 0; i < tags.length; i++) {
+			tid = tags[i].getAttribute('enumID');
+			type = tags[i].getAttribute('type');
+			value = tags[i].getAttribute('value');
+			text = tags[i].textContent;
+
+			key = TagMap[tid];	
+			if (!key) continue;
+			obj[key] = (type == 'String') ? text : parseInt(value);
+		}
+		return obj;
+	}
+
+	var content = Fs.readFileSync(args[0], 'utf-8'),
+	    xmlDoc = (new DOMParser()).parseFromString(content, 'text/xml'),
+	    entities = xmlDoc.getElementsByTagName('Entity'),
+	    objs = [], obj;
+	for (var i = 0; i < entities.length; i++) {
+		obj = node2obj(entities[i]);
+		objs.push(obj);
+	}	
+
+	console.log('var _infoList =');
+	console.log(objs);
+	console.log(';');
+})();
