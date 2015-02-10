87531b5cc4bdc8c43a4f8f56a67308e48fe48de2
chinesedfan <chinesedfan@qq.com>
2015-02-10 16:58:29 +0800

    refactor database.js

--- js/database.js
+++ js/database.js
@@ -5,35 +5,94 @@
        Mac OS X: ~/Library/Application\ Support/Google/Chrome/Default/databases
        Linux: ~/.config/google-chrome/Default/databases
 */
-function DbConn() {
-    this._db = openDatabase("hsdb", "1.0", "HearthStone Records", "4096");
+function DbConn(opts) {
+    var arena = opts ? opts.arena || "arena" : "arena",
+        packs = opts ? opts.packs || "packs" : "packs",
+        lacks = opts ? opts.lacks || "lacks" : "lacks";
 
-    window._onSqlError = this._onSqlError; // tricky solution
+    this._db = openDatabase("hsdb", "1.0", "HearthStone Records", "4096");
+    
+    // prepare sqls
+    this.sqlCreateArena = "CREATE TABLE IF NOT EXISTS " + arena + "(id integer PRIMARY KEY UNIQUE,day date,class varchar,wins integer)";
+    this.sqlCreatePacks = "CREATE TABLE IF NOT EXISTS " + packs + "(id integer PRIMARY KEY UNIQUE,day date,count_gl integer,count_ge integer,count_gr integer,count_gc integer,count_l integer,count_e integer,count_r integer,count_c integer,tip_gl text,tip_ge text,tip_gr text,tip_gc text,tip_l text,tip_e text,tip_r text,tip_c text,dust integer)";
+    this.sqlCreateLacks = "CREATE TABLE IF NOT EXISTS " + lacks + "(id integer PRIMARY KEY AUTOINCREMENT UNIQUE,card_id integer,card_name text,card_quality integer)";
+
+    this.sqlLoadArenaData = "SELECT * FROM " + arena;
+    this.sqlInsertArenaRow = "INSERT INTO " + arena + "(id, day, class, wins) VALUES(?, ?, ?, ?)";
+    this.sqlDeleteArenaById = "DELETE FROM " + arena + " WHERE id = ?";
+
+    this.sqlLoadPacksData = "SELECT * FROM " + packs + "";
+    this.sqlInsertPacksRow = "INSERT INTO " + packs + " VALUES(?,?, ?,?,?,?,?,?,?,?, ?,?,?,?,?,?,?,?, ?)";
+    this.sqlDeletePacksById = "DELETE FROM " + packs + " WHERE id = ?";
+
+    this.sqlLoadLacksData = "SELECT * FROM " + lacks + "";
+    this.sqlInsertLacksRow = "INSERT INTO " + lacks + "(card_id, card_name, card_quality) VALUES(?,?,?)";
+    this.sqlDeleteLacksById = "DELETE FROM " + lacks + " WHERE id IN (SELECT min(id) FROM lacks WHERE card_id = ?)";
+
+    // init the database
+    this.execSqls([this.sqlCreateArena, this.sqlCreatePacks, this.sqlCreateLacks]);
 }
 
 DbConn.prototype = {
+    constructor: DbConn,
+
     _onSqlError: function(tx, error) {
         alert("[onSqlError]" + error.message);
     },
 
-    execSqlScript: function(script) {
-        this._db.transaction(function(tx) {
-            var sqls = script.split(';\n'),
-                n = sqls.length, success = 0;
-            for (var i = 0; i < sqls.length; i++) {
-                tx.executeSql(sqls[i], [], function(tx, rs) {
+    execSql: function(sql, args, onSucc, onErr) {
+        var self = this;
+        
+        self._db.transaction(function(tx) {
+            tx.executeSql(sql, args || [], onSucc, onErr || self._onSqlError);
+        });
+    },
+    execSqls: function(sqls) {
+        var self = this,
+            i, n = sqls.length, success = 0;
+        
+        self._db.transaction(function(tx) {
+            for (i = 0; i < n; i++) {
+                tx.executeSql(sqls[i], args[i] || [], function(tx, rs) {
                     console.log("[execSqlScript]" + (++success) + "/" + n);
-                }, this._onSqlError);
+                }, self._onSqlError);
             }
         });
     },
+    execSqlScript: function(script) {
+        var sqls = script.split(';\n');
 
-    initDB: function() {
-        this._db.transaction(function(tx) {
-            tx.executeSql("CREATE TABLE IF NOT EXISTS arena(id integer PRIMARY KEY UNIQUE,day date,class varchar,wins integer)", [], undefined, this._onSqlError);
-            tx.executeSql("CREATE TABLE IF NOT EXISTS packs(id integer PRIMARY KEY UNIQUE,day date,count_gl integer,count_ge integer,count_gr integer,count_gc integer,count_l integer,count_e integer,count_r integer,count_c integer,tip_gl text,tip_ge text,tip_gr text,tip_gc text,tip_l text,tip_e text,tip_r text,tip_c text,dust integer)", [], undefined, this._onSqlError);
-            tx.executeSql("CREATE TABLE IF NOT EXISTS lacks(id integer PRIMARY KEY AUTOINCREMENT UNIQUE,card_id integer,card_name text,card_quality integer)", [], undefined, this._onSqlError);
-        });
+        this.execSqls(sqls);
+    },
+
+    loadArenaData: function(callback) {
+        this.execSql(this.sqlLoadArenaData, [], callback);
+    },
+    insertArenaRow: function(row, callback) {
+        this.execSql(this.sqlInsertArenaRow, row, callback);
+    },
+    deleteArenaById: function(id, callback) {
+        this.execSql(this.sqlDeleteArenaById, [id], callback);
+    },
+
+    loadPacksData: function(callback) {
+        this.execSql(this.sqlLoadPacksData, [], callback);
+    },
+    insertPacksRow: function(row, callback) {
+        this.execSql(this.sqlInsertPacksRow, row, callback);
+    },
+    deletePacksById: function(id, callback) {
+        this.execSql(this.sqlDeletePacksById, [id], callback);
+    },
+
+    loadLacksData: function(callback) {
+        this.execSql(this.sqlLoadLacksData, [], callback);
+    },
+    insertLacksRow: function(row, callback) {
+        this.execSql(this.sqlInsertLacksRow, row, callback);
+    },
+    deleteLacksById: function(id, callback) {
+        this.execSql(this.sqlDeleteLacksById, [id], callback);
     },
 
     loadArenaData: function(page) {
