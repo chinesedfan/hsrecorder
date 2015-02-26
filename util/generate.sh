e22593093b71410eef872dfe21f3efa28f6c32ad
chinesedfan <chinesedfan@qq.com>
2015-02-26 16:47:43 +0800

    support simplified cards info, #23

--- util/generate.sh
+++ util/generate.sh
@@ -7,11 +7,11 @@ OUTPUT_DIR=../bin
 # Open {$HS_HOME}/Hearthstone.app/Contents/Data/Managed/Assembly-CSharp.dll by Monodevelop's assembly
 # browser(Download from: http://www.monodevelop.com/), find enum GAME_TAG in namespace -, then copy all
 # contents into this file **MANUALLY**.
-TAG_FILE=$OUTPUT_DIR/GameTag.cs
+TAG_FILE=GameTag.cs
 # Covert C# enums into a js map.
 TAG_MAP_FILE=$OUTPUT_DIR/tagmap.js
 # The final result is saved into this file.
-INFO_FILE=$OUTPUT_DIR/info.js
+INFO_FILE=$OUTPUT_DIR/cardlist.js
 
 # The directory where you install the game.
 HS_HOME=/Applications/Hearthstone
