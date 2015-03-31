e7062d59fe7b2ed985c04e1912552d4d31461111
chinesedfan <chinesedfan@qq.com>
2015-03-31 15:10:53 +0800

    trie tree draft

--- js/cards.js
+++ js/cards.js
@@ -1,51 +1,53 @@
-var Key2Card = (function() {
-	var prefixMap = {},
-		name, key, i, j;
+function TrieNode(letter) {
+	this.letter = letter;
+	this.cards = [];
+	this.children = {};
+}
 
-	// only classify by prefixes that are shorter than three
+var Key2Card = (function() {
+	var root = new TrieNode(''), node,
+		name, letter;
+
+	// build a trie tree
 	CardList.forEach(function(card, i) {
 		name = card.CARDNAME.toLowerCase();
-		for (var i = 0; i < 3; i++) {
-			key = name.substring(0, i + 1);
+		node = root;
 
-			if (!(key in prefixMap)) prefixMap[key] = [];
-
-			if (prefixMap[key].length < GameConst.SUGGEST_ITEM_MAX) {
-				prefixMap[key].push(card);
+		for (i = 0; i < name.length; i++) {
+			letter = name[i];
+			if (!node.children[letter]) {
+				node.children[letter] = new TrieNode(letter);
 			}
+			node = node.children[letter];
 		}
+
+		node.cards.push(card);
 	});
 
-	// if candidates are not enough, consider discontinuous substrings
-	for (key in prefixMap) {
-		if (prefixMap[key].length == GameConst.SUGGEST_ITEM_MAX) continue;
-
-		appendDiscontinuous(key);	
-	}
-
-	function appendDiscontinuous(key) {
-		CardList.forEach(function(card) {
-			name = card.CARDNAME.toLowerCase(), i = 0, j = 0;
-			while (i < key.length && j < name.length) {
-				while (name[j] != key[i]) { j++; if (j == name.length) break; };
-				if (j == name.length) break;
-
-				i++; j++;
-			}
-			if (i == key.length && card ) {
-				prefixMap[key].push(card);
-				if (prefixMap[key].length == GameConst.SUGGEST_ITEM_MAX) return false;
-			}
-		});
-	}
-
 	function _getCards(key) {
-		if (!prefixMap[key]) {
-			prefixMap[key] = [];
-			appendDiscontinuous(key);
+		var queue = [root], result = [];
+
+		root.position = 0;
+		// travel the tree level by level
+		while(queue.length) {
+			node = queue.shift();
+
+			for (letter in node.children) {
+				node.children[letter].position = node.position;
+				queue.push(node.children[letter]);
+			}
+
+			if (node.position < key.length) {
+				letter = key[node.position];
+				if (node.children[letter]) {
+					node.children[letter].position = node.position + 1;
+				}
+			} else {
+				result.concat(node.cards);
+			}
 		}
 
-		return prefixMap[key];
+		return result;
 	}
 
 	return {
